7f24e12201f5a13e9de026f97ac8a9ad13c19da9
vu <duyvujarvan4@gmail.com>
2022-07-04 11:18:20 +0700

    update

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ module.exports = withImages({
   reactStrictMode: true,
   images: {
     disableStaticImages: true,
-    domains: [
-      "quanly.exxe.vn",
-      "n1-cstg.mioto.vn",
-      "res.cloudinary.com",
-      process.env.NEXT_PUBLIC_IMAGE_URL,
-    ],
+    domains: ["quanly.exxe.vn", "n1-cstg.mioto.vn", "res.cloudinary.com"],
   },
 })
